18e74a954018fd30da776e68d2fad54863a50b0c
RAKHYUN <kyle2598@gmail.com>
2023-10-16 10:01:01 -0500

    Initial Commit

---
+++ CitySightApp2/CitySightApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightApp2App.swift
+//  CitySightApp2
+//
+//  Created by Rakhyun Kim on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CitySightApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
